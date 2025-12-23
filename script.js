b4c601d22fbba46515b4b192acf673b70a09074a
mounika-10782 <cuite.mounika@gmail.com>
2025-12-23 10:48:08 +0530

    Add LearnSphere frontend implementation (HTML, CSS, JS)

---
+++ script.js
@@ -0,0 +1,9 @@
+// Function to open the success modal
+function openModal() {
+    document.getElementById('successModal').style.display = 'flex'; // Show the modal
+  }
+  
+  // Function to close the success modal
+  function closeModal() {
+    document.getElementById('successModal').style.display = 'none'; // Hide the modal
+  }
